1484c524cbca26adc925902587631eab7bc04938
Stella Kim <testcodesk@gmail.com>
2019-10-22 16:42:11 -0400

    Working!

--- src/server/app.js
+++ src/server/app.js
@@ -3,16 +3,19 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
-const jwt = require('jsonwebtoken')
 const authController = require('../controllers/auth-controller')
 const registerController = require('../controllers/register-controller')
-const {findUser, findByToken} = require('../server/model/User') 
 const verify = require("./routes/verifyToken");
+const session = require('express-session')
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(express.json());
-
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
  app.get('/signup', function (req, res) {  
     res.sendFile(path.join(__dirname, '../view/register.html'));
@@ -22,20 +25,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../view/login.html'));
 }) 
 
-app.get('/api/:token', (req, res)=> {
-  const token = req.params['token'].split('=')[1];
-  console.log(token)
-  jwt.verify(token, process.env.JWT_SECRET, (err, authData) => {
-    if(err){
-      res.sendStatus(403)
-    }else{
-      res.status(200).sendFile(path.join(__dirname, '../view/home.html'))
-    }
-  })
-})
-
-app.get('/api', (req,res) => {
-  res.write('Welcome to the GreenConnect API');
+app.get('/api', verify, (req,res) => {
+    res.status(200).sendFile(path.join(__dirname, '../view/home.html'))
 })
 
 /* route to handle login and registration */
@@ -47,30 +38,8 @@ app.post('/controllers/authenticate-controller', authController.authenticate);
 
 /* Log out */
 app.get('/logout', (req, res, next) => {
-    // res.end(`You are logged out.`)
+    req.session.destroy();
     res.redirect("/")
 })
-
-
-// app.post('/api/sessions', (req, res, next) => {
-//     const user = req.body;
-
-//     findUser(user.email) 
-//     .then((users) => {
-//       if (users.length > 0 ) {
-//         const token = jwt.encode({id: user.id}, process.env.JWT_SECRET)
-//         return res.status(200).send({ token });
-//       }
-//       throw { status: 401 };
-//     })
-//     .catch((err) => next(err));
-// });
-
-// app.get('/api/sessions', (req, res,next) => {
-//   if(req.headers.authorization){
-//     res.end();
-//   } 
-//   next({status:401})
-// })
 
 module.exports = app
