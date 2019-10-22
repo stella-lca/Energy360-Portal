be068065a9ff5323611950da9f13dc2d08e1d906
Stella Kim <testcodesk@gmail.com>
2019-10-22 13:51:59 -0400

    required fields

--- src/server/routes/verifyToken.js
+++ src/server/routes/verifyToken.js
@@ -1,35 +1,16 @@
 const {findByToken} = require('../../server/model/User') 
-const jwt = require('jsonwebtoken')
 
-module.exports = async function(req, res, next){
-    // Get auth header value
-    const bearerHeader = req.headers['authorization'];
-    // Check if bearer is undefined
-    if(typeof bearerHeader !== 'undefined') {
-    // Split at the space
-    // Get token from array
+module.exports = function(req, res, next){
+  const bearerHeader = req.headers['authorization'];
+
+  if(typeof bearerHeader !== 'undefined') {
     const bearerToken = bearerHeader.split(' ')[1];
     // Set the token
-    req.query.token = bearerToken;
-    // Next middleware
+    req.token = bearerToken;
     next();
   } else {
     // Forbidden
-    res.sendStatus(403);
+    res.sendStatus(403)
   }
-
-
-    // const token = req.header('authorization');
-    // // const token = req.headers.authorization
-
-    // if(!token) return res.status(401).send('Access Denied');
-    
-    // try{
-    //     const user = await findByToken(authorization)
-    //     req.user = user
-    //     next()
-    // }catch(ex){
-    //     res.status(400).send('Invalid Token');
-    // }
 } 
 
