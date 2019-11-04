6df85f5f890fd1748627776e3ce807e3ed6a7410
stella-lca <55708308+stella-lca@users.noreply.github.com>
2019-11-04 18:25:58 -0500

    Update redirect-back-controller.js

--- src/controllers/redirect-back-controller.js
+++ src/controllers/redirect-back-controller.js
@@ -3,24 +3,25 @@ const {updateUser} = require('../server/model/User')
 
 module.exports.redirectBack = async function(req, res){
     const {scope} = req.body;
-
+    const scopes = scope.join("|");
+    
     if(req.session !== undefined && req.session.user !== undefined ){
-      await updateUser(req.session.user.id, {scope})
+      await updateUser(req.session.user.id, {scopes})
 
       const utilityProvider = req.session.user.accountTypeDetail;
       const ceconyRedirectBackURL = `https://www.coned.com/accounts-billing/dashboard/billing-and-usage/share-my-data-connections/third-party-authorization/redirect?`
                                   +`client_id=${process.env.APPSETTING_CLIENT_ID}`
-                                  +`&scope=${scope}`
+                                  +`&scope=${scopes}`
       const oruRedirectBackURL = `https://www.oru.com/accounts-billing/dashboard/billing-and-usage/share-my-data-connections/third-party-authorization/redirect?`
                                   +`client_id=${process.env.APPSETTING_CLIENT_ID}`
-                                  +`&scope=${scope}`
+                                  +`&scope=${scopes}`
       
       if(utilityProvider === 'CECONY'){
-        res.send('This will be redirect to '+ ceconyRedirectBackURL)
-        // res.redirect(ceconyRedirectBackURL)
+//      res.send('This will be redirect to '+ ceconyRedirectBackURL)
+        res.redirect(ceconyRedirectBackURL)
       } else if(utilityProvider === 'ORU'){
-        res.send('This will be redirect to '+ oruRedirectBackURL)
-        // res.redirect(oruRedirectBackURL)
+//         res.send('This will be redirect to '+ oruRedirectBackURL)
+        res.redirect(oruRedirectBackURL)
       }
     } else {
       res.status(403).send(`Forbidden. Please login to use GreenConnect.`)
