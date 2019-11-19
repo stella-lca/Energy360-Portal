1a8c262d230bc59155c8a86f87d52a6735b22b17
Stella Kim <stella@cutone.org>
2019-11-18 23:07:37 -0500

    oru url

--- src/controllers/redirect-back-controller.js
+++ src/controllers/redirect-back-controller.js
@@ -4,7 +4,7 @@ const {updateUser} = require('../server/model/User')
 module.exports.redirectBack = async function(req, res){
     const {scope} = req.body;
     const scopes = (Array.isArray(scope))? scope.join("|") : scope;
-    
+
     if(req.session !== undefined && req.session.user !== undefined ){
       await updateUser(req.session.user.id, {scope:scopes})
 
@@ -12,7 +12,7 @@ module.exports.redirectBack = async function(req, res){
       const ceconyRedirectBackURL = `https://wem-cm-t1.coned.com/accounts-billing/dashboard/billing-and-usage/share-my-data-connections/third-party-authorization/redirect?`
                                   +`client_id=${process.env.APPSETTING_CLIENT_ID}`
                                   +`&scope=${scopes}`
-      const oruRedirectBackURL = `https://wem-cm-t1.coned.com/accounts-billing/dashboard/billing-and-usage/share-my-data-connections/third-party-authorization/redirect?`
+      const oruRedirectBackURL = `https://wem-cm-t1.oru.com/accounts-billing/dashboard/billing-and-usage/share-my-data-connections/third-party-authorization/redirect?`
                                   +`client_id=${process.env.APPSETTING_CLIENT_ID}`
                                   +`&scope=${scopes}`
       
