c4313ae6c774187bc9eee23928aedfdf2a5f0906
jsdev63 <react.dev@yahoo.com>
2020-02-06 22:00:29 +0100

    implemented webpack project

---
+++ config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'Energy360-Web-Portal',
+  port: 3000,
+  apiKey: '2AgDahoJLeL3aDXfTebhTqS4I2YTC72v'
+}
--- server.js
+++ server.js
@@ -5,29 +5,35 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const app = express()
 
-const appConfig = require('./app.config.js')
+const {port} = require('./config')
 
 app.use(helmet())
 
 // Express Route File Requires
 const __apiName = require('./api/name.js')
+const __apiUser = require('./api/user.js')
 
 // <REQUIRES> DON'T CHANGE THIS LINE - Express route file requires will be added above here.
 
-app.use(express.static(path.resolve(__dirname, 'src/public')))
+app.use(express.static(path.resolve(__dirname, 'dist')))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // Express Routing
 app.use('/api/name', __apiName)
+app.use('/api/user', __apiUser)
 
 // <ROUTING-E> DON'T CHANGE THIS LINE - Express routing will be added above here.
 
 // React Routing
+app.use('/*?', express.static(path.resolve((__dirname, 'dist'))))
 
 // <ROUTING-R> DON'T CHANGE THIS LINE - React routing will be added above here.
 
-http.createServer(app).listen(appConfig.port)
+
+http.createServer(app).listen(port, ()=>{
+   console.log(`Server running at http://localhost:${port}/`);
+})
 
 module.exports = app
