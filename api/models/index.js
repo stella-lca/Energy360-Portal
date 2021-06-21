00684d0fde709a16434382353c206eb144aab898
jsdev63 <jsdev63@yahoo.com>
2021-06-21 06:14:26 -0600

    fixed forgot password error

--- api/models/index.js
+++ api/models/index.js
@@ -1,24 +1,24 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const {
-	SQLAZURECONNSTR_DB_HOST,
-	SQLAZURECONNSTR_DB_USER,
-	SQLAZURECONNSTR_DB_PW,
-	SQLAZURECONNSTR_DB_NAME
+  SQLAZURECONNSTR_DB_HOST,
+  SQLAZURECONNSTR_DB_USER,
+  SQLAZURECONNSTR_DB_PW,
+  SQLAZURECONNSTR_DB_NAME
 } = process.env;
 
 const db = {};
-
+trustServerCertificate: true;
 const sequelize = new Sequelize(
-	SQLAZURECONNSTR_DB_NAME,
-	SQLAZURECONNSTR_DB_USER,
-	SQLAZURECONNSTR_DB_PW,
-	{
-		host: SQLAZURECONNSTR_DB_HOST,
-		dialect: "mssql",
-		dialectOptions: { options: { encrypt: true } }
-	}
+  SQLAZURECONNSTR_DB_NAME,
+  SQLAZURECONNSTR_DB_USER,
+  SQLAZURECONNSTR_DB_PW,
+  {
+    host: SQLAZURECONNSTR_DB_HOST,
+    dialect: 'mssql',
+    dialectOptions: { options: { encrypt: true, trustServerCertificate: true } }
+  }
 );
 
 // sequelize.getQueryInterface().showAllTables().then(function(tables) {
@@ -26,12 +26,12 @@ const sequelize = new Sequelize(
 // });
 
 Sequelize.DATE.prototype._stringify = function _stringify(date, options) {
-	return this._applyTimezone(date, options).format("YYYY-MM-DD HH:mm:ss.SSS");
+  return this._applyTimezone(date, options).format('YYYY-MM-DD HH:mm:ss.SSS');
 };
 
-db.User = sequelize.import("./user.model");
-db.Token = sequelize.import("./token.model");
-db.Log = sequelize.import("./log.model");
+db.User = sequelize.import('./user.model');
+db.Token = sequelize.import('./token.model');
+db.Log = sequelize.import('./log.model');
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
