8127401b6f804377879c45007400b875fdd2c229
stella-lca <stella@cutone.org>
2025-12-18 16:44:45 -0600

    fix package and installation

--- src/hooks/ForgotPassword.js
+++ src/hooks/ForgotPassword.js
@@ -1,84 +1,96 @@
 import React, { useState, useContext } from "react";
 import {
-	Container,
-	Button,
-	Row,
-	Col,
-	Card,
-	Form,
-	Input,
-	FormGroup,
-	Alert
+  Container,
+  Button,
+  Row,
+  Col,
+  Card,
+  Form,
+  Input,
+  FormGroup,
+  Alert
 } from "reactstrap";
 import authUtils from "../utils/auth";
 import { ContextState } from "../context";
 import Loading from "../assets/img/loading_spinner.gif";
 
 const ForgotPassword = () => {
-	const { message, isloading } = useContext(ContextState);
-	const [errors, setError] = useState(false);
-	const { forgotPassword } = authUtils();
+  const { message = {}, isloading } = useContext(ContextState);
+  const [error, setError] = useState("");
+  const { forgotPassword } = authUtils();
 
-	const onSigninSubmit = e => {
-		e.preventDefault();
-		let emailValue = e.target.elements.email.value;
-		if (!emailValue) {
-			setError("Please enter your email!");
-		} else {
-			forgotPassword(emailValue);
-		}
-	};
-	console.log(message, isloading);
-	return (
-		<Container>
-			<Row>
-				<Col className="middle-container ml-auto mr-auto" lg="6">
-					<Card className="card-signup reset-password ml-auto mr-auto my-auto">
-						{isloading ? (
-							<div className="loading-image">
-								<img src={`/${Loading}`} width="100" height="100" />
-							</div>
-						) : (
-							<>
-								<div className="title mx-auto">
-									<h2>Forgot Password</h2>
-								</div>
-								<Form className="reset-password-form" onSubmit={onSigninSubmit}>
-									{!message.status && message.status !== undefined ? (
-										<Alert color="danger">{message.msg}</Alert>
-									) : null}
-									{message.status ? (
-										<h4>
-											We already sent the forgot password Email. <br />
-											Please check your email!
-										</h4>
-									) : (
-										<>
-											<p>
-												Enter your email address and your password will be reset
-												and emailed to you.
-											</p>
-											<label>Email Address</label>
-											<Input
-												placeholder="Enter email"
-												type="email"
-												name="email"
-											/>
-											<FormGroup className="button-group">
-												<Button block className="btn-round" color="danger">
-													Request Password Change
-												</Button>
-											</FormGroup>
-										</>
-									)}
-								</Form>
-							</>
-						)}
-					</Card>
-				</Col>
-			</Row>
-		</Container>
-	);
+  const onSigninSubmit = e => {
+    e.preventDefault();
+    const emailValue = e.target.elements.email.value;
+
+    if (!emailValue) {
+      setError("Please enter your email address");
+      return;
+    }
+    setError("");
+    forgotPassword(emailValue);
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col className="middle-container ml-auto mr-auto" lg="6">
+          <Card className="card-signup reset-password ml-auto mr-auto my-auto">
+
+            {isloading ? (
+              <div className="loading-image text-center">
+                <img src={Loading} width="100" height="100" alt="loading" />
+              </div>
+            ) : (
+              <>
+                <div className="title mx-auto">
+                  <h2>Forgot Password</h2>
+                </div>
+
+                <Form className="reset-password-form" onSubmit={onSigninSubmit}>
+                  {error && <Alert color="danger">{error}</Alert>}
+                  {message?.status === false && (
+                    <Alert color="danger">{message.msg}</Alert>
+                  )}
+
+                  {message?.status === true ? (
+                    <h4 className="text-center">
+                      We already sent the reset email.  
+                      <br />Please check your inbox.
+                    </h4>
+                  ) : (
+                    <>
+                      <p>
+                        Enter your email address and a password reset link will be sent.
+                      </p>
+
+                      <label>Email Address</label>
+                      <Input
+                        name="email"
+                        type="email"
+                        placeholder="Enter email"
+                      />
+
+                      <FormGroup className="button-group">
+                        <Button
+                          type="submit"
+                          block
+                          className="btn-round"
+                          color="danger"
+                        >
+                          Request Password Change
+                        </Button>
+                      </FormGroup>
+                    </>
+                  )}
+                </Form>
+              </>
+            )}
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default ForgotPassword;
